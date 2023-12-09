7b1936153799898bae52b77acbd977fb4e56644b
MORISSEAU Albin <amu2244@baratheon.enseeiht.fr>
2023-12-09 17:20:04 +0100

    message

--- src/commands.adb
+++ src/commands.adb
@@ -1,32 +1,14 @@
-with Ada.Text_IO;            use Ada.Text_IO;
-with Ada.Strings.Unbounded;  use Ada.Strings.Unbounded;
+with Ada.IO_Exceptions;
+with Ada.Text_IO;			use Ada.Text_IO;
+with Ada.Float_Text_IO;		use Ada.Float_Text_IO;
+with Ada.Integer_Text_IO;	use Ada.Integer_Text_IO;
+with Ada.Command_line;		use Ada.Command_line;
+with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 
 package body commands is
 
-    function Parcourir(commande: in String) return T_Tableau is
-        tableau: T_Tableau;
-        k: Integer;
-    begin
-        k := 1;
-        for i in 1..commande'Last loop
-            if commande(i) = '-' then
-                tableau(k).Nom_Argument := commande(i+1);
-                if commande(i+1)='P'|commande(i+1)='C' then
-                    tableau(k).Argument := 0;
-                else
-                    while commande(i) /= '-' loop
-                        tableau(k).Argument := float;
-                 end if;
-            k := k+1;
-            end if;
-        end loop;
-        return tableau;
-    end Parcourir;
-
-
-
-    procedure Traiter_argument(commande: in String; alpha: in out Long_Float ; k: in out Integer ; epsilon: in out Long_Float ; creuse: in out Boolean ; pleine: in out Boolean ; prefixe: in out Unbounded_String; fichier_graphe: in out Unbounded_String ) is
-    Aide: Unbounded_String;
+    procedure Traiter_argument(commande: in Unbounded_String; alpha: in out Long_Float ; k: in out Integer ; epsilon: in out Long_Float ; creuse: in out Boolean ; pleine: in out Boolean ; prefixe: in out Unbounded_String; fichier_graphe: in out Unbounded_String ) is
+    i : Integer;
     begin
         -- Initialisation des variables avec les valeurs par défaut imposées par l'énoncé --
         alpha := 0.85;
@@ -36,26 +18,64 @@ package body commands is
         pleine := False;
         prefixe := To_Unbounded_String ("output");
         fichier_graphe := To_Unbounded_String ("");
-        Aide := To_Unbounded_String ("");
+        i:=1;
 
+        -- on est obligé d'utiliser un while car on pourrait pas modiifer l'indice de parcours i à notre guise avec un if --
+        while i <= Argument_Count loop
+            -- on ne peut pas faire de case car Argument(i) est une String  donc un type non discret --
+ --
+            if Argument (i) = "-A" then
+                alpha := Long_Float'Value(Argument(i+1));
+                i := i+2;
+            elsif Argument (i) = "-K" then
+                k := Integer'Value(Argument(i+1));
+                i := i+2;
+            elsif Argument (i) = "-P" then
+                pleine := True;
+                i := i+1;
+            elsif Argument (i) = "-C" then
+                i := i+1;
+            elsif Argument (i)= "-E" then
+                epsilon := Long_Float'Value(Argument(i+1));
+                i := i+2;
+            elsif Argument (i)= "-R" then
+                prefixe := To_Unbounded_String(Argument(i+1));
+                i := i+2;
+            elsif Argument (i) = "" then
+                fichier_graphe := To_Unbounded_String(Argument(i+1));
+                i := i+2;
+            end if;
 
-        Case Nom_Argument is
-           when "A" => alpha := Argument;
-           when "K" => k := argument;
-           when "E" => epsilon  := Argument;
-           when "P" => creuse := true;
-           when "C" => pleine := false;
-           when "R" => prefixe := Argument;
-           when others => if fichier_graphe = "" then
-                        fichier_graphe := Argument;
-                     else
-                        Put("Cet argument n’existe pas");
-                        Put(Aide);
-                    end if;
-            end Case;
+        end loop;
     end Traiter_argument;
 
-    procedure Validite(alpha: in out Integer ; k: in out Integer ; epsilon: in out Long_Float ; creuse: in out Boolean ; pleine: in out Boolean ; prefixe: in out Unbounded_String; fichier_graphe: in out Unbounded_String ) is
-        Null;
+    procedure Validite(argument: in String; alpha: in out Long_Float ; k: in out Integer ; epsilon: in out Long_Float ; creuse: in out Boolean ; pleine: in out Boolean) is
+    begin
+        -- on ne peut pas faire de case car Argument(i) est une String  donc un type non discret --
+            if argument="alpha" then
+                if Long_Float'Value(alpha)<Long_Float'Value(0) or Long_Float'Value(alpha)>Long_Float'Value(1)  then
+                    Put("Alpha doit être compris entre 0 et 1 au sens large");
+                    raise Error_Argument;
+                end if;
+            elsif argument="epsilon" then
+                 if epsilon<0 then
+                    Put("epsilon  doit être positif");
+                    raise Error_Argument;
+                end if;
+            elsif argument="matrice" then
+                if pleine=creuse then
+                    Put("Vous ne pouvez pas choisir à la fois le mode matrice pleine et le mode matrice creuse");
+                    raise Error_Argument;
+                end if;
+            elsif argument="k" then
+                if k<0 then
+                    Put("k  doit être positif");
+                    raise Error_Argument;
+                end if;
+           else
+                Put("Il faut spécifier un fichier d'entrée");
+                raise Error_Argument;
+            end if;
 
+    end Validite;
 end commands;
--- src/commands.ads
+++ src/commands.ads
@@ -1,25 +1,12 @@
 with Ada.Strings.Unbounded;     use Ada.Strings.Unbounded;
 
-package commands is
+package commands is 
     
-    type T_couple is private;
-    type T_Tableau is private;
-    
-    -- On parcourt une chaine de caractère pour identifier les différents arguments
-    function Parcourir(commande: in String) return T_Tableau;
+    Error_Argument: Exception;
     
     -- On traite les arguments appelés par l'utilisateur --
-    procedure Traiter_argument(alpha: in out Long_Float ; k: in out Integer ; epsilon: in out Long_Float ; creuse: in out Boolean ; pleine: in out Boolean ; prefixe: in out Unbounded_String; fichier_graphe: in out Unbounded_String );
+    procedure Traiter_argument(commande: in Unbounded_String; alpha: in out Long_Float ; k: in out Integer ; epsilon: in out Long_Float ; creuse: in out Boolean ; pleine: in out Boolean ; prefixe: in out Unbounded_String; fichier_graphe: in out Unbounded_String );
 
-    procedure Validite(alpha: in out Integer ; k: in out Integer ; epsilon: in out Long_Float ; creuse: in out Boolean ; pleine: in out Boolean ; prefixe: in out Unbounded_String; fichier_graphe: in out Unbounded_String);
-    
-private 
-    type T_couple is record
-        Nom_Argument: Character;
-        Argument: Unbounded_String;
-    end record;
-    -- Invariants --
-    type T_Tableau is array(1..6) of T_couple; 
-    -- Invariants --
+    procedure Validite(argument: in String; alpha: in out Long_Float ; k: in out Integer ; epsilon: in out Long_Float ; creuse: in out Boolean ; pleine: in out Boolean);
     
 end commands;
