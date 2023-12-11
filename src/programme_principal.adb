520d3d059773055a646c0b01076fa2907fdff4e4
AlbinMorisseau <151684782+AlbinMorisseau@users.noreply.github.com>
2023-12-11 17:42:57 +0100

    programme_principal.adb

--- src/programme_principal.adb
+++ src/programme_principal.adb
@@ -9,10 +9,60 @@ with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 procedure Programme_Principal is
 
 begin
-    package body commands is
+
+    -- Initilisation des exceptions
+    Argument_Error : exception;
+    No_Argument_Error : exception;
+    Mauvais_Argument_Error : exception;
+
+    procedure Help is
+    begin
+        Put_Line("Fonctionnement du programme PageRank.");
+        New_Line;
+        Put_Line("Il y a une erreur dans la saisie de vos arguments.");
+        New_Line;
+        Put_Line("Arguments :");
+        New_Line;
+        Put_Line("-A <valeur>");
+        Put_Line(" Vous devez renseigner une valeur réelle de alpha comprise entre 0 et 1 .");
+        Put_Line(" La pondération alpha est appelée le dumping factor. Plus sa valeur est proche de 1, plus la convergence du calcul de la matrice de Google est lente.");
+        Put_Line("Par défaut cette valeur de alpha est fixée à 0.85 .");
+        New_Line;
+        Put_Line("-K <valeur>");
+        Put_Line(" Vous devez renseigner une valeur de k entière positive.");
+        Put_Line(" k est l'indice du vecteur poids à calculer.");
+        Put_Line("Par défaut cette valeur de k est fixée à 150.");
+        New_Line;
+        Put_Line("-E <valeur>");
+        Put_Line(" Vous devez renseigner une valeur de epsilon entière positive.");
+        Put_Line(" epsilon est la précision qui permettra d’interrompre le calcul du Page-Rank si le vecteur poids est à ");
+        Put_Line("une distance du vecteur poids précédent strictement inférieureà epsilon.");
+        Put_Line("Par défaut cette valeur de epsilon est nulle.");
+        New_Line;
+        Put_Line ("-R <prefixe>");
+        Put_Line ("Vous devez renseigner le prefixe de votre fichier que l'algorithme va vous renvoyer (nom du fichier de sortie).");
+        Put_Line ("Par défaut cette valeur de prefixe est output.");
+        New_Line;
+        Put_Line ("-P");
+        Put_Line ("Il n'y a pas de valeur d'argument à donner.");
+        Put_Line ("Permet de choisir l'algorithme avec des matrices pleines (la matrice G est calculée).");
+        Put_Line ("Attention, vous ne pouvez pas activer à al fois le mode creuse et à la fois le mode pleine");
+        New_Line;
+        Put_Line ("-C");
+        Put_Line ("Il n'y a pas de valeur d'argument à donner.");
+        Put_Line ("Permet de choisir l'algorithme avec des matrices creuses (la matrice G n'est pas calculée).");
+        Put_Line ("Attention, vous ne pouvez pas activer à al fois le mode creuse et à la fois le mode pleine");
+        Put_Line ("Cet algorithme est celui executé par défaut.");
+        New_Line;
+        Put_Line(" En fin de ligne de commande indiquez le fichier graphe que vous voulez fournir au programme ");
+    end Help;
+
+
 
     procedure Traiter_argument(commande: in Unbounded_String; alpha: in out Long_Float ; k: in out Integer ; epsilon: in out Long_Float ; creuse: in out Boolean ; pleine: in out Boolean ; prefixe: in out Unbounded_String; fichier_graphe: in out Unbounded_String ) is
-    i : Integer;
+        i : Integer; -- indice de notre boucle while
+        cas_option : String (1..2);
+        cas_option_car: Character;
     begin
         -- Initialisation des variables avec les valeurs par défaut imposées par l'énoncé --
         alpha := 0.85;
@@ -24,65 +74,66 @@ begin
         fichier_graphe := To_Unbounded_String ("");
         i:=1;
 
-        -- on est obligé d'utiliser un while car on pourrait pas modiifer l'indice de parcours i à notre guise avec un if --
-        while i <= Argument_Count loop
-            -- on ne peut pas faire de case car Argument(i) est une String  donc un type non discret --
- --
-            if Argument (i) = "-A" then
-                alpha := Long_Float'Value(Argument(i+1));
-                i := i+2;
-            elsif Argument (i) = "-K" then
-                k := Integer'Value(Argument(i+1));
-                i := i+2;
-            elsif Argument (i) = "-P" then
-                pleine := True;
-                i := i+1;
-            elsif Argument (i) = "-C" then
-                i := i+1;
-            elsif Argument (i)= "-E" then
-                epsilon := Long_Float'Value(Argument(i+1));
-                i := i+2;
-            elsif Argument (i)= "-R" then
-                prefixe := To_Unbounded_String(Argument(i+1));
-                i := i+2;
-            elsif Argument (i) = "" then
-                fichier_graphe := To_Unbounded_String(Argument(i+1));
-                i := i+2;
-            end if;
+        begin
+        -- Vérifier que le nom du fichier est fourni et le récupérer
+	    if Argument_Count < 1 then
+		    raise No_Argument_Error;
+         else
+                fichier_graphe := To_Unbounded_String(Argument(Argument_Count));
+         end if;
 
+        for i in 1..Argument_Count-1 loop
+            cas_option := Argument(i);
+            if  cas_option'Length=2 and cas_option(1) = '-' then
+                cas_option_car := cas_option(2);
+                case cas_option_car is
+                    when 'A' =>
+                        if Float'Value(Argument(i+1)) >=0.0 and Float'Value(Argument(i+1)) <= 1.0 then
+                            alpha:= Long_Float'Value(Argument(i+1));
+                        else
+                            Put_Line("Vous devez respecter les conditions sur alpha");
+                            raise Argument_Error;
+                        end if;
+                    when 'E' =>
+                        if Float'Value(Argument(i+1)) >=0.0 then
+                            epsilon := Long_Float'Value(Argument(i+1));
+                        else
+                             Put_Line("Vous devez respecter les conditions sur epsilon");
+                            raise Argument_Error;
+                        end if;
+                    when 'K' =>
+                        if Float'Value(Argument(i+1)) >=0.0 then
+                            k := Integer'Value(Argument(i+1));
+                        else
+                            Put_Line("Vous devez respecter les conditions sur k");
+                            raise Argument_Error;
+                        end if;
+                    when 'C' =>
+                        creuse := True;
+                        if pleine = True then
+                            Put_Line ("Attention, vous ne pouvez pas activer à al fois le mode creuse et à la fois le mode pleine");
+                            raise Argument_Error;
+                        end if;
+                    when 'P' =>
+                        pleine := True;
+                    when 'R' =>
+                        prefixe := To_Unbounded_String((i+1));
+                    when others =>
+                        raise Mauvais_Argument_Error;
+                end case;
+            end if;
         end loop;
-    end Traiter_argument;
 
-    procedure Validite(argument: in String; alpha: in out Long_Float ; k: in out Integer ; epsilon: in out Long_Float ; creuse: in out Boolean ; pleine: in out Boolean) is
-    begin
-        -- on ne peut pas faire de case car Argument(i) est une String  donc un type non discret --
-            if argument="alpha" then
-                if Long_Float'Value(alpha)<Long_Float'Value(0) or Long_Float'Value(alpha)>Long_Float'Value(1)  then
-                    Put("Alpha doit être compris entre 0 et 1 au sens large");
-                    raise Error_Argument;
-                end if;
-            elsif argument="epsilon" then
-                 if epsilon<0 then
-                    Put("epsilon  doit être positif");
-                    raise Error_Argument;
-                end if;
-            elsif argument="matrice" then
-                if pleine=creuse then
-                    Put("Vous ne pouvez pas choisir à la fois le mode matrice pleine et le mode matrice creuse");
-                    raise Error_Argument;
-                end if;
-            elsif argument="k" then
-                if k<0 then
-                    Put("k  doit être positif");
-                    raise Error_Argument;
-                end if;
-           else
-                Put("Il faut spécifier un fichier d'entrée");
-                raise Error_Argument;
-            end if;
+        exception
+            when Argument_Error =>
+                Help;
+            when Mauvais_Argument_Error =>
+                Put_Line(" Vous devez renseigner uniqument les arguments pris en charge par le programme ");
+                Help;
+        end;
 
-    end Validite;
-end commands;
+
+     end Traiter_argument;
     Algorithme_PageRank(0.85, 150, 0.0, true, "testoutput", "exemple-fichier.txt");
 
 end Programme_Principal;
