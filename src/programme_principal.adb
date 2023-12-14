aa7c4e20db6af1d3fd8b6d909b801faadfa88286
Louis THEVENET <louis.tvnt@gmail.Com>
2023-12-14 10:24:37 +0100

    Mis les variables en majuscule + corrigé un oubli de Argument

--- src/programme_principal.adb
+++ src/programme_principal.adb
@@ -22,9 +22,9 @@ procedure Programme_Principal is
         Put_Line("Arguments :");
         New_Line;
         Put_Line("-A <valeur>");
-        Put_Line(" Vous devez renseigner une valeur réelle de alpha comprise entre 0 et 1 .");
-        Put_Line(" La pondération alpha est appelée le dumping factor. Plus sa valeur est proche de 1, plus la convergence du calcul de la matrice de Google est lente.");
-        Put_Line("Par défaut cette valeur de alpha est fixée à 0.85 .");
+        Put_Line(" Vous devez renseigner une valeur réelle de Alpha comprise entre 0 et 1 .");
+        Put_Line(" La pondération Alpha est appelée le dumping factor. Plus sa valeur est proche de 1, plus la convergence du calcul de la matrice de Google est lente.");
+        Put_Line("Par défaut cette valeur de Alpha est fixée à 0.85 .");
         New_Line;
         Put_Line("-K <valeur>");
         Put_Line(" Vous devez renseigner une valeur de k entière positive.");
@@ -32,37 +32,40 @@ procedure Programme_Principal is
         Put_Line("Par défaut cette valeur de k est fixée à 150.");
         New_Line;
         Put_Line("-E <valeur>");
-        Put_Line(" Vous devez renseigner une valeur de epsilon entière positive.");
-        Put_Line(" epsilon est la précision qui permettra d’interrompre le calcul du Page-Rank si le vecteur poids est à ");
-        Put_Line("une distance du vecteur poids précédent strictement inférieureà epsilon.");
-        Put_Line("Par défaut cette valeur de epsilon est nulle.");
+        Put_Line(" Vous devez renseigner une valeur de Epsilon entière positive.");
+        Put_Line(" Epsilon est la précision qui permettra d’interrompre le calcul du Page-Rank si le vecteur poids est à ");
+        Put_Line("une distance du vecteur poids précédent strictement inférieureà Epsilon.");
+        Put_Line("Par défaut cette valeur de Epsilon est nulle.");
         New_Line;
-        Put_Line ("-R <prefixe>");
-        Put_Line ("Vous devez renseigner le prefixe de votre fichier que l'algorithme va vous renvoyer (nom du fichier de sortie).");
-        Put_Line ("Par défaut cette valeur de prefixe est output.");
+        Put_Line ("-R <Prefixe>");
+        Put_Line ("Vous devez renseigner le Prefixe de votre fichier que l'algorithme va vous renvoyer (nom du fichier de sortie).");
+        Put_Line ("Par défaut cette valeur de Prefixe est output.");
         New_Line;
         Put_Line ("-P");
         Put_Line ("Il n'y a pas de valeur d'argument à donner.");
-        Put_Line ("Permet de choisir l'algorithme avec des matrices pleines (la matrice G est calculée).");
-        Put_Line ("Attention, vous ne pouvez pas activer à al fois le mode creuse et à la fois le mode pleine");
+        Put_Line ("Permet de choisir l'algorithme avec des matrices Pleines (la matrice G est calculée).");
+        Put_Line ("Attention, vous ne pouvez pas activer à al fois le mode Creuse et à la fois le mode Pleine");
         New_Line;
         Put_Line ("-C");
         Put_Line ("Il n'y a pas de valeur d'argument à donner.");
-        Put_Line ("Permet de choisir l'algorithme avec des matrices creuses (la matrice G n'est pas calculée).");
-        Put_Line ("Attention, vous ne pouvez pas activer à al fois le mode creuse et à la fois le mode pleine");
+        Put_Line ("Permet de choisir l'algorithme avec des matrices Creuses (la matrice G n'est pas calculée).");
+        Put_Line ("Attention, vous ne pouvez pas activer à al fois le mode Creuse et à la fois le mode Pleine");
         Put_Line ("Cet algorithme est celui executé par défaut.");
         New_Line;
         Put_Line(" En fin de ligne de commande indiquez le fichier graphe que vous voulez fournir au programme ");
+        New_line;
+        Put_Line("Exemple de commande : ./PageRank -A 0.85 -K 150 -E 0.0001 -C -R output graphe.txt");
+
     end Help;
 
     -- Initialiser les variables
-        alpha : Long_Float;
-        k : Integer;
-        epsilon : Long_Float;
-        creuse : Boolean;
-        pleine : Boolean;
-        prefixe : Unbounded_String;
-        fichier_graphe : Unbounded_String;
+        Alpha : Long_Float;
+        K : Integer;
+        Epsilon : Long_Float;
+        Creuse : Boolean;
+        Pleine : Boolean;
+        Prefixe : Unbounded_String;
+        Fichier_graphe : Unbounded_String;
 
             i : Integer; -- indice de notre boucle while
         cas_option : String (1..2);
@@ -72,13 +75,13 @@ procedure Programme_Principal is
 
 
         -- Initialisation des variables avec les valeurs par défaut imposées par l'énoncé --
-        alpha := 0.85;
+        Alpha := 0.85;
         k := 150;
-        epsilon := 0.0;
-        creuse := True;
-        pleine := False;
-        prefixe := To_Unbounded_String ("output");
-        fichier_graphe := To_Unbounded_String ("");
+        Epsilon := 0.0;
+        Creuse := True;
+        Pleine := False;
+        Prefixe := To_Unbounded_String ("output");
+        Fichier_graphe := To_Unbounded_String ("");
         i:=1;
 
         begin
@@ -86,7 +89,7 @@ procedure Programme_Principal is
 	    if Argument_Count < 1 then
             raise No_Argument_Error;
          else
-                fichier_graphe := To_Unbounded_String(Argument(Argument_Count));
+                Fichier_graphe := To_Unbounded_String(Argument(Argument_Count));
          end if;
 
         for i in 1..Argument_Count-1 loop
@@ -98,45 +101,45 @@ procedure Programme_Principal is
                 case cas_option_car is
                     when 'A' =>
                         if Float'Value(Argument(i+1)) >=0.0 and Float'Value(Argument(i+1)) <= 1.0 then
-                            alpha:= Long_Float'Value(Argument(i+1));
+                            Alpha:= Long_Float'Value(Argument(i+1));
                         else
-                            Put_Line("Vous devez respecter les conditions sur alpha");
+                            Put_Line("Vous devez respecter les conditions sur Alpha");
                             New_Line;
                             raise Argument_Error;
                         end if;
                     when 'E' =>
                         if Float'Value(Argument(i+1)) >=0.0 then
-                            epsilon := Long_Float'Value(Argument(i+1));
+                            Epsilon := Long_Float'Value(Argument(i+1));
                         else
-                            Put_Line("Vous devez respecter les conditions sur epsilon");
+                            Put_Line("Vous devez respecter les conditions sur Epsilon");
                             New_Line;
                             raise Argument_Error;
                         end if;
                     when 'K' =>
                         if Float'Value(Argument(i+1)) >=0.0 then
-                            k := Integer'Value(Argument(i+1));
+                            K := Integer'Value(Argument(i+1));
                         else
                             Put_Line("Vous devez respecter les conditions sur k");
                             New_Line;
                             raise Argument_Error;
                         end if;
                     when 'C' =>
-                        creuse := True;
-                        if pleine = True then
-                            Put_Line ("Attention, vous ne pouvez pas activer à la fois le mode creuse et à la fois le mode pleine");
+                        Creuse := True;
+                        if Pleine = True then
+                            Put_Line ("Attention, vous ne pouvez pas activer à la fois le mode Creuse et à la fois le mode Pleine");
                             New_Line;
                             raise Argument_Error;
                         end if;
                     when 'P' =>
-                        pleine := True;
+                        Pleine := True;
                     when 'R' =>
-                        prefixe := To_Unbounded_String((i+1));
+                        Prefixe := To_Unbounded_String(Argument(i+1));
                     when others =>
                         raise Mauvais_Argument_Error;
                 end case;
             end if;
         end loop;
-            Algorithme_PageRank(alpha, k, epsilon, pleine, To_String(prefixe), To_String(fichier_graphe));
+            Algorithme_PageRank(Alpha, K, Epsilon, Pleine, To_String(Prefixe), To_String(Fichier_graphe));
 
         exception
         when Argument_Error
