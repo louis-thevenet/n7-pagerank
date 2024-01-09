4b9c2d98faa30c1aa2b91c6ad7def9697712e570
Louis THEVENET <louis.tvnt@gmail.Com>
2024-01-09 17:55:00 +0100

    nix: adds build dependencies + tools

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "A Nix flake dev environment for N7 assignements (Matlab, Coq, Gnat, X2GO, ...)";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells = {
+        default = pkgs.mkShell {
+
+
+          packages = with pkgs; [
+            # Nix
+            nil
+            alejandra
+
+            # Ada
+            gnat
+            gprbuild
+            valgrind
+            gdb
+            hyperfine
+          ];
+        };
+      };
+    });
+}
